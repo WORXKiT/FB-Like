f88cb35912549ac35514938d77e6ddea3d1b0189
krittakon357 <krittakon159@gmail.com>
2018-07-27 16:29:21 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 sudo apt update
 sudo apt-get install python3-pip
 sudo pip3 install virtualenv
+virtualenv -p /usr/bin/python2.7 venv
 source ./venv/bin/activate
 pip freeze > requirements.txt
 pip install -r requirements.txt
